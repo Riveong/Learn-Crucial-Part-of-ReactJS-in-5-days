347bad845e9273aee8c40a0fffb21abb21bdbc14
Ong Riverine <setsubi9@gmail.com>
2023-07-05 17:05:44 +0700

    cumit

---
+++ react101/src/Message.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+//PascalCasin
+function Message(){
+    //JSX (Javascript XML)
+    const name = "aaa";
+    if(name)
+    return <h1>hello {name}</h1>;
+}
+
+//default object of the module
+export default Message;
